a5bf315581e3f3b0d680200de455cd064af4e01f
HorningT <thomas.horning@colorado.edu>
2020-11-16 14:29:30 -0700

    Update Converter.py

--- Converter.py
+++ Converter.py
@@ -12,10 +12,8 @@ def PDFconvertGC(filename):
     # Imports
     from google.colab import drive; from datetime import datetime; import sys; 
     import os; import time
-
     # Mount Drive First to give time for mounting to process
     drive.mount('/content/gdrive', force_remount = True) 
-
     # Install some dependences into the RUNTIME (is not local, needs to reinstall 
     # every Factory runtime)
     t1 = time.time()
@@ -25,18 +23,15 @@ def PDFconvertGC(filename):
     os.system('install nbconvert >> outputsuppressed.txt')
     os.system('pip install jupyter >> outputsuppressed.txt')
     os.system('apt-get install texlive-xetex texlive-fonts-recommended texlive-generic-recommended >> outputsuppressed.txt')
-    t2 = time.time(); m = str(int((t2-t1)/60)); s = str(int((t2-t1)% 60))
-    print("Install Time: ",m," m  ",s," s")
-
     # Searches the Google drive directory for the filename and gives back it's 
     # location (This accounts for Wildcards and Spaces in the directory names).
     # Uses jupyter and nbconvert to convert to a Tex file, then into a pdf 
-
     # Handle Common Errors
     try:
-        print('\nFinding file. This may take a minute or two depending on the size of your drive...\n')
+        print('\nFinding file. This may take a minute or two depending on the size of your drive...')
         os.system("IFS=$'\n'") #Sets the reader to only break at newlines instead of spaces, tabs,and newlines
         loc = os.path.abspath(filename)
+        
     except IndexError as error:
         print(color.BOLD,color.FAIL, "\nCould not find file in your Drive!\n" 
             ,color.END,color.WARNING
@@ -47,7 +42,8 @@ def PDFconvertGC(filename):
                                         ,sys.exc_info()[2].tb_lineno))
         f = open("ErrorLog.txt","a+"); f.write(Er); f.close()
         sys.tracebacklimit=0
-        sys.exit("Please Try Again") 
+        sys.exit("Please Try Again")
+        
     except Exception as exception:
         print(color.BOLD,color.WARNING, "Exception Occured, Please Check Log",color.END)
         Er = str('Error: {}. {}, line: {}'.format(sys.exc_info()[0],sys.exc_info()[1]
@@ -55,22 +51,16 @@ def PDFconvertGC(filename):
         f = open("ErrorLog.txt","a+");f.write(Er);f.close()
         sys.tracebacklimit=0
         sys.exit("Please Try Again")
-    print('File Location: ',str(loc),'\n')
-    t3 = time.time(); 
-    m = str(int((t3-t2)/60)); 
-    s = str(int((t3-t2)% 60))
-    print("Search Time: ",m," m  ",s," s")
+        
+    print('File Found at: ',str(loc))
    # Autosave file
-    os.system('sleep 60s')
+    os.system('sleep 30s')
    # Convert the file
     os.system('jupyter nbconvert --to pdf "{fileloc}" --log-level ERROR')
     # The PDF will be in the same folder as the original file
     print(color.GREEN,"Conversion Complete!\nGreat Job and Have a Wonderful Day!"
             ,color.END,"\U0001F30C")
     t4 = time.time(); 
-    m = str(int((t4-t3)/60));
-    s = str(int((t4-t3)% 60))
-    print("Conversion Time: ",m," m  ",s," s")
     m = str(int((t4-t1)/60)); 
     s = str(int((t4-t1)% 60))
     print("Total Time: ",m," m  ",s," s")
